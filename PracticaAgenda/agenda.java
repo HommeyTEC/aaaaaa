7722c91b1269d1865a788bf1ab8340c79a001e41
HommeyTEC <hommeyjosueochoa@gmail.com>
2025-02-11 11:52:40 -0700

    Add files via upload

--- PracticaAgenda/agenda.java
+++ PracticaAgenda/agenda.java
@@ -32,7 +32,31 @@ public String getTelefono() {
 public void setTelefono(String telefono) {
    Telefono=telefono;
 }
-
+public static void Registrar(Contacto[] agenda,String nombre) {
+	String numero = "";
+	boolean contacto_invalido = false;
+	boolean Hecho = false;
+	do {
+		if(AgendaLlena(agenda)) 
+			break;
+		contacto_invalido= ExisteContacto(agenda,nombre);
+		if(contacto_invalido==true) {
+			System.out.println("Contacto ya registrado");
+			Hecho=true;
+			break;
+		}
+		for(int k=0;k<agenda.lenght;k++) {
+			if(agenda[k].getNombre()=="*") {
+				System.out.println("Telefono del contacto #"+(k+1)+": ");
+				numero=Leer.dato();
+				if(numero.lenght()>10) {
+					numero=numero.substring(0,10);
+				}
+			}
+		}
+		
+	}
+}
 
 
 }
--- PracticaAgenda/main.java
+++ PracticaAgenda/main.java
@@ -2,10 +2,19 @@ package PracticaAgenda;
 public class main {
 	
 public static void main(String[] args) {
+	String nombre ="";
 	agenda[] Contacto = new agenda[10];
+	for(int i=0;i<Contacto.length;i++) {
+		Contacto[i].setNombre("*");
+	}
+	int Opcion=0;
+	int A;
+	String N="";
+	boolean Existe=false;
+	do {
     System.out.println("Agenda Telefonica!!");
     System.out.println("----------------------");
-    System.out.println("1. Añadir contacto");
+    System.out.println("1. Anadir contacto");
     System.out.println("2. Eliminar contacto");
     System.out.println("3. Consultar contacto");
     System.out.println("4. Listar agenda");
@@ -13,13 +22,24 @@ public static void main(String[] args) {
     System.out.println("6. Salir de la agenda");
     System.out.println("-----------------------");
     System.out.println("Selecciona una opcion ");
+    switch(Opcion) {
+    case 1: 
+    	System.out.print("Nombre del contacto: ");
+    	nombre=Leer.dato();
+    	Registrar(A,nombre);
+    	break;
+    case 2:
+    	EliminarContacto(A);
+    	break;
+    }
     
     
     
     
     
     
-
-
+	}while(true);
+	
+	}
 }
 
